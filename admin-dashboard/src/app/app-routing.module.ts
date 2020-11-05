8a4ebe62cd776380a6e37938da38f9bfaf2916f7
Nguyen Phuong Nam (RBVH/ETM44) <nna7hc@bosch.com>
2020-11-05 22:51:01 +0700

    original

--- admin-dashboard/src/app/app-routing.module.ts
+++ admin-dashboard/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductListComponent } from './product/product-list/product-list.component';
-import { AddProductComponent } from './product/add-product/add-product.component';
+// import { ProductListComponent } from './product/product-list/product-list.component';
+// import { AddProductComponent } from './product/add-product/add-product.component';
 
 const routes: Routes = [
-  { path: 'product', component: ProductListComponent },
-  { path: 'product/add-product', component: AddProductComponent}
+  // { path: 'product', component: ProductListComponent },
+  // { path: 'product/add-product', component: AddProductComponent}
 ];
 
 @NgModule({
--- admin-dashboard/src/app/app.module.ts
+++ admin-dashboard/src/app/app.module.ts
@@ -7,16 +7,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatTableModule, MatPaginatorModule, MatSortModule  } from '@angular/material';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AppRoutingModule } from './app-routing.module';
-import { ProductListComponent } from './product/product-list/product-list.component';
-import { AddProductComponent } from './product/add-product/add-product.component';
+// import { ProductListComponent } from './product/product-list/product-list.component';
+// import { AddProductComponent } from './product/add-product/add-product.component';
 import { ReactiveFormsModule  } from '@angular/forms'
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
-    ProductListComponent,
-    AddProductComponent
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
